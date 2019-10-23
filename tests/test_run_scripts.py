47aa14dbb3708e3474aa8ba5a5e639a8fde786b8
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-10-22 18:37:32 -0700

    Added unit tests for the run scripts

---
+++ tests/test_run_scripts.py
@@ -0,0 +1,37 @@
+import os
+import unittest
+
+from ray.tune import run as run_tune
+
+from utils.parsers import env_parser, init_parser
+from run_scripts.random_rollout import run as run_random
+from run_scripts.test_rllib_script import setup_exps
+
+class TestRollout(unittest.TestCase):
+
+    def test_rollout_random(self):
+        parser = init_parser()
+        args = env_parser(parser).parse_args()
+        passed_config = {'config_path': args.env_config, 'policy_config': args.policy_config,
+                         'policy': args.policy, 'show_images': args.show_images,
+                         'train_on_images': True}
+        run_random(passed_config)
+        passed_config = {'config_path': args.env_config, 'policy_config': args.policy_config,
+                         'policy': args.policy, 'show_images': args.show_images,
+                         'train_on_images': False}
+        run_random(passed_config)
+
+    def test_run_script(self):
+        alg_run, config, exp_dict, args = setup_exps()
+
+        # save the relevant params for replay
+        exp_dict['config']['train_batch_size'] = 500
+        exp_dict['stop']['training_iteration'] = 1
+        exp_dict['config']['env_config'] = {'policy': args.policy, 'show_images': False,
+                                            'train_on_images': False,
+                                            'config_path': args.env_config, 'policy_config': args.policy_config}
+        run_tune(**exp_dict, queue_trials=False)
+
+
+if __name__ == '__main__':
+    unittest.main()
