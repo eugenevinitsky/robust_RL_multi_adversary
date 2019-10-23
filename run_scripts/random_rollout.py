f4bce7c234fb4dd94ccfe0d0e85bec282958c123
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-10-22 18:37:32 -0700

    Added unit tests for the run scripts

--- run_scripts/random_rollout.py
+++ run_scripts/random_rollout.py
@@ -3,29 +3,17 @@
 import argparse
 import configparser
 import os
+import sys
 
 import numpy as np
 
 from envs.crowd_env import CrowdSimEnv
 from envs.policy.policy_factory import policy_factory
 from envs.utils.robot import Robot
+from utils.parsers import env_parser
 
-def main():
-    parser = argparse.ArgumentParser('Parse configuration file')
-    script_path = os.path.dirname(os.path.abspath(__file__))
 
-    parser.add_argument('--env_config', type=str, default=os.path.abspath(os.path.join(script_path,'../configs/env.config')))
-    parser.add_argument('--policy_config', type=str, default=os.path.abspath(os.path.join(script_path,'../configs/policy.config')))
-    parser.add_argument('--policy', type=str, default='cadrl')
-    parser.add_argument('--train_config', type=str, default=os.path.join(script_path,'../configs/train.config'))
-    parser.add_argument("--show_images", action="store_true", default=False, help="Whether to display the observations")
-    parser.add_argument('--train_on_images', action='store_true', default=False, help='Whether to train on images')
-
-    args = parser.parse_args()
-
-    passed_config = {'config_path': args.env_config, 'policy_config': args.policy_config,
-                                'policy': args.policy, 'show_images': args.show_images, 'train_on_images': args.train_on_images}
-
+def run(passed_config):
     config_path = passed_config['config_path']
     temp_config = configparser.RawConfigParser()
     temp_config.read(config_path)
@@ -43,15 +31,24 @@ def main():
     policy_config.read(passed_config['policy_config'])
     policy = policy_factory[passed_config['policy']](policy_config)
     if not policy.trainable:
-        parser.error('Policy has to be trainable')
-    if args.policy_config is None:
-        parser.error('Policy config has to be specified for a trainable network')
+        sys.exit('Policy has to be trainable')
+    if passed_config["policy_config"] is None:
+        sys.exit('Policy config has to be specified for a trainable network')
 
     robot.set_policy(policy)
 
     ob = env.reset()
     for i in range(100):
         ob, rew, done, info = env.step(np.random.rand(2))
+
+
+def main():
+
+    args = env_parser().parse_args()
+    passed_config = {'config_path': args.env_config, 'policy_config': args.policy_config,
+                     'policy': args.policy, 'show_images': args.show_images, 'train_on_images': args.train_on_images}
+    run(passed_config)
+
 
 if __name__ == "__main__":
     main()    
