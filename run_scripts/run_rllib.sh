6eb599979adc5e63c1a7ad71e8d6731c32db1607
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-11-06 12:26:24 -0800

    start autoencoder

--- run_scripts/run_rllib.sh
+++ run_scripts/run_rllib.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python test_rllib_script.py --exp_title test_sim2real --num_cpus 8 --num_iters 1
+python test_rllib_script.py --exp_title test_sim2real --num_cpus 8 --num_iters 100
