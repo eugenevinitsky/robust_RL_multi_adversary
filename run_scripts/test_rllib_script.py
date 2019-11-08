9f821641d8157efa2c883a021a7e664e4812c018
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-11-08 10:51:02 -0800

    More changes for exp running

--- run_scripts/test_rllib_script.py
+++ run_scripts/test_rllib_script.py
@@ -27,7 +27,7 @@ def setup_exps(args):
     config = ppo.DEFAULT_CONFIG.copy()
     config['num_workers'] = args.num_cpus
     config['gamma'] = 0.99
-    #config['train_batch_size'] = 10000
+    config['train_batch_size'] = 30000
 
     with open(args.env_params, 'r') as file:
         env_params = file.read()
@@ -58,7 +58,9 @@ def setup_exps(args):
         config['model']['custom_model'] = "rnn"
         
         config['vf_share_layers'] = True
-        config['train_batch_size'] = 30000
+        config['sgd_minibatch_size'] = 600
+        config['num_sgd_iter'] = 10
+        config['train_batch_size'] = 5000
 
     s3_string = 's3://sim2real/' \
                 + datetime.now().strftime('%m-%d-%Y') + '/' + args.exp_title
--- train_on_cluster.sh
+++ train_on_cluster.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ray exec autoscale.yaml "python adversarial_sim2real/test_rllib_script.py --exp_title test_memory --num_cpus 32 --num_iters 100" \
-    --start --stop --cluster-name=test_memory
+ray exec autoscale.yaml "python adversarial_sim2real/run_scripts/test_rllib_script.py --exp_title test_memory --num_cpus 32 --num_iters 300 --train_on_images" \
+    --start --stop --cluster-name=test_memory --tmux
