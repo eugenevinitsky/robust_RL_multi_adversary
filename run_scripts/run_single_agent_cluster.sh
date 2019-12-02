88d9f061c4b0881f4dac3cc3223993de5c658ff0
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-12-02 00:54:42 -0500

    Remove the unnecessary stop command

--- run_scripts/run_single_agent_cluster.sh
+++ run_scripts/run_single_agent_cluster.sh
@@ -34,18 +34,18 @@
 ####################################################################################################
 # 12/1/19 running the exps with 4 humans to attempt to encourage it to avoid humans
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/test_rllib_script.py --exp_title SA_h4 --use_s3 \
---train_batch_size 30000 --checkpoint_freq 50 --num_iters 500 --num_cpus 18 --run_transfer_tests --human_num 4" --tmux --start --stop --cluster-name=sa_test1
+--train_batch_size 30000 --checkpoint_freq 50 --num_iters 500 --num_cpus 18 --run_transfer_tests --human_num 4" --tmux --start --cluster-name=sa_test1
 
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/test_rllib_script.py --exp_title SA_GaS_h4 --use_s3 \
 --train_batch_size 30000 --checkpoint_freq 50 --num_iters 500 --add_gaussian_noise_state --num_cpus 18 --run_transfer_tests --human_num 4" \
---tmux --start --stop --cluster-name=sa_test2
+--tmux --start --cluster-name=sa_test2
 
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/test_rllib_script.py --exp_title SA_GaA_h4 --use_s3 \
 --train_batch_size 30000 --checkpoint_freq 50 --num_iters 500 --add_gaussian_noise_action --num_cpus 18 --run_transfer_tests --human_num 4" \
---tmux --start --stop --cluster-name=sa_test3
+--tmux --start --cluster-name=sa_test3
 
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/test_rllib_script.py --exp_title SA_GaS_GaA_h4 --use_s3 \
 --train_batch_size 30000 --checkpoint_freq 50 --num_iters 500 --add_gaussian_noise_state --add_gaussian_noise_action --num_cpus 18 --run_transfer_tests --human_num 4" \
---tmux --start --stop --cluster-name=sa_test4
+--tmux --start --cluster-name=sa_test4
 
 
