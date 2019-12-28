b5f7e6742e50781ebdc419b9124b2f96c429445c
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-12-28 17:51:46 -0500

    Update transfer tests to be parallel

--- visualize/transfer_test.py
+++ visualize/transfer_test.py
@@ -24,211 +24,109 @@ def reset_config(rllib_config):
     return copy_config
 
 
-def run_transfer_tests(rllib_config, checkpoint, num_rollouts, output_file_name, outdir, save_trajectory, show_images=False):
-    # TODO(@Evinitsky) when you run the transfer tests for multi-agent you want to run it with no adversaries!!!
-    file_path = os.path.dirname(os.path.abspath(__file__))
+@ray.remote
+def run_test(test_name, is_env_config, config_value, params_name, params_value, outdir, output_file_name,
+             save_trajectory, show_images, num_rollouts,
+             rllib_config, checkpoint):
+    """Run an individual transfer test
 
+    Parameters
+    ----------
+    test_name: (str)
+        Name of the test we are running. Used to find which env param to set to True
+    is_env_config: (bool)
+        If true we write the param into the env config so that it is actually updated correctly since some of the
+        env configuration is done through the env_params.config
+    config_value: (bool or str or None)
+        This is the value we will insert into the env config. For example, True, or "every_step" or so on
+    params_name: (str or None)
+        If is_env_config is true, this is the key into which we will store things
+    params_value: (bool or str or None)
+        If is_env_config is true, this is the value we will store into the env_params.config
+    outdir: (str)
+        Directory results are saved to
+    output_file_name: (str)
+        Prefix string for naming the files. Used to uniquely identify experiments
+    save_trajectory: (bool)
+        If true, a video will be saved for this rollout
+    show_images: (bool)
+        If true, a render of the rollout will be displayed on your machine
+    num_rollouts: (int)
+        How many times to rollout the test. Increasing this should yield more stable results
+    rllib_config: (dict)
+        Passed rllib config
+    checkpoint: (int)
+        Number of the checkpoint we want to replay
+    """
+    file_path = os.path.dirname(os.path.abspath(__file__))
     # First compute a baseline score to compare against
     print(
         "**********************************************************\n"
         "**********************************************************\n"
         "**********************************************************\n"
-        "Running the baseline score!\n"
+        "Running the {} score!\n"
         "**********************************************************\n"
         "**********************************************************\n"
-        "**********************************************************"
+        "**********************************************************".format(test_name)
     )
     temp_config = reset_config(rllib_config)
-    base_rewards, num_steps_base = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory, video_file="base",
-                               show_images=show_images, num_rollouts=num_rollouts)
-    output_file_path = os.path.join(file_path, '{}/{}_base.txt'.format(outdir, output_file_name))
-    if not os.path.exists(os.path.dirname(output_file_path)):
+    if is_env_config:
+        params_string = temp_config['env_config']['env_params']
+        params_parser = configparser.RawConfigParser()
+        params_parser.read_string(params_string)
+        params_parser['sim'][params_name] = str(params_value)
+        with open('tmp.config', 'w') as file:
+            params_parser.write(file)
+        with open('tmp.config', 'r') as file:
+            env_params = file.read()
+        os.remove('tmp.config')
+        temp_config['env_config']['env_params'] = env_params
+    # the base case is the only one we don't write update an env param for
+    if 'test_name' != 'base' and not is_env_config:
+        temp_config['env_config'][test_name] = config_value
+
+    rewards, steps = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory,
+                                               video_file=test_name,
+                                               show_images=show_images, num_rollouts=num_rollouts)
+
+    with open(os.path.join(file_path, '{}/{}_{}_rew.txt'.format(outdir, output_file_name, test_name)),
+              'wb') as file:
+        np.savetxt(file, rewards, delimiter=', ')
+    with open(os.path.join(file_path, '{}/{}_{}_steps.txt'.format(outdir, output_file_name, test_name)),
+              'wb') as file:
+        np.savetxt(file, steps, delimiter=', ')
+
+    print('The average {} reward, episode length is {}, {}'.format(test_name, np.mean(rewards), np.mean(steps)))
+
+
+def run_transfer_tests(rllib_config, checkpoint, num_rollouts, output_file_name, outdir, save_trajectory, show_images=False):
+
+    file_path = os.path.dirname(os.path.abspath(__file__))
+    output_file_path = os.path.join(file_path, outdir)
+    if not os.path.exists(output_file_path):
         try:
             os.makedirs(os.path.dirname(output_file_path))
         except OSError as exc:
             if exc.errno != errno.EEXIST:
                 raise
 
-    with open(os.path.join(file_path, '{}/{}_base_rew.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, base_rewards, delimiter=', ')
-    with open(os.path.join(file_path, '{}/{}_base_steps.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, num_steps_base, delimiter=', ')
+    # test name, is_env_config, config_value, params_name, params_value
+    run_list = [
+        ['base', False, None, None, None],
+        ['friction', False, True, None, None],
+        ['change_colors_mode', False, 'ever_step', None, None],
+        ['restrict_goal_region', True, False, 'randomize_goals', True],
+        ['chase_robot', False, True, None, None],
+        ['add_gaussian_noise_state', False, True, None, None],
+        ['add_gaussian_noise_action', False, True, None, None]
+    ]
 
-    # First lets do the friction rewards
-    print(
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "Running the friction score!\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************"
-    )
-    temp_config = reset_config(rllib_config)
-    temp_config['env_config']['friction'] = True
-    friction_rewards, num_steps_friction = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory, video_file="friction",
-                                   show_images=show_images, num_rollouts=num_rollouts)
-    with open(os.path.join(file_path, '{}/{}_friction_rew.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, friction_rewards, delimiter=', ')
-    with open(os.path.join(file_path, '{}/{}_friction_steps.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, num_steps_friction, delimiter=', ')
-
-    # Now change the colors
-    print(
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "Running the colors score!\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************"
-    )
-    temp_config = reset_config(rllib_config)
-    temp_config['env_config']['change_colors_mode'] = 'every_step'
-    color_rewards, num_steps_colors = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory, video_file="colours",
-                                show_images=show_images,  num_rollouts=num_rollouts)
-    with open(os.path.join(file_path, '{}/{}_color_rew.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, color_rewards, delimiter=', ')
-    with open(os.path.join(file_path, '{}/{}_color_steps.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, num_steps_colors, delimiter=', ')
-
-    # Allow goals to spawn anywhere (no longer restricted to smaller space)
-    print(
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "Running the changing goals score!\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************"
-    )
-
-    # undo the goal restriction
-    temp_config = reset_config(rllib_config)
-    temp_config['env_config']['restrict_goal_region'] = False
-
-    # enable goal randomization. We need to do it in this complicated way because
-    # this param is in the env_params.config instead of the env config
-    # TODO(@evinitsky) make this less gross
-    params_string = temp_config['env_config']['env_params']
-    params_parser = configparser.RawConfigParser()
-    params_parser.read_string(params_string)
-    params_parser['sim']['randomize_goals'] = str(True)
-    with open('tmp.config', 'w') as file:
-        params_parser.write(file)
-    with open('tmp.config', 'r') as file:
-        env_params = file.read()
-    os.remove('tmp.config')
-    temp_config['env_config']['env_params'] = env_params
-    unrestrict_goal_reg_rewards, num_steps_unrestrict = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory, video_file="goal_reg",
-                                              show_images=show_images, num_rollouts=num_rollouts)
-    with open(os.path.join(file_path, '{}/{}_unrestrict_goal_reg_rew.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, unrestrict_goal_reg_rewards, delimiter=', ')
-    with open(os.path.join(file_path, '{}/{}_unrestrict_goal_reg_steps.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, num_steps_unrestrict, delimiter=', ')
-
-    # Have humans chase robot (hand-tuned adversarial human behaviour"
-    print(
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "Running the humans chasing agent score!\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************"
-    )
-    temp_config = reset_config(rllib_config)
-    temp_config['env_config']['chase_robot'] = True
-    chase_robot_rewards, num_steps_chase = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory, video_file="chase_rob",
-                                      show_images=show_images, num_rollouts=num_rollouts)
-    with open(os.path.join(file_path, '{}/{}_chase_robot_rew.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, chase_robot_rewards, delimiter=', ')
-    with open(os.path.join(file_path, '{}/{}_chase_robot_steps.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, num_steps_chase, delimiter=', ')
-
-    # Add gaussian noise to the state
-    print(
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "Running the gaussian state noise score!\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************"
-    )
-    temp_config = reset_config(rllib_config)
-    temp_config['env_config']['add_gaussian_noise_state'] = True
-    gaussian_state_rewards, num_steps_GaS = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory, video_file="gauss_state",
-                                         show_images=show_images, num_rollouts=num_rollouts)
-    with open(os.path.join(file_path, '{}/{}_gaussian_state_noise_rew.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, gaussian_state_rewards, delimiter=', ')
-    with open(os.path.join(file_path, '{}/{}_gaussian_state_noise_steps.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, num_steps_GaS, delimiter=', ')
-
-    # Add gaussian noise to the actions
-    print(
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "Running the gaussian action noise score!\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************"
-    )
-    temp_config = reset_config(rllib_config)
-    temp_config['env_config']['add_gaussian_noise_action'] = True
-    gaussian_action_rewards, num_steps_GaA = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory, video_file="gauss_ac",
-                                          show_images=show_images, num_rollouts=num_rollouts)
-    with open(os.path.join(file_path, '{}/{}_gaussian_action_noise_rew.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, gaussian_action_rewards, delimiter=', ')
-    with open(os.path.join(file_path, '{}/{}_gaussian_action_noise_steps.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, num_steps_GaA, delimiter=', ')
-
-    # Add gaussian noise to the states and actions
-    print(
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "Running the gaussian state and action noise score!\n"
-        "**********************************************************\n"
-        "**********************************************************\n"
-        "**********************************************************"
-    )
-    temp_config = reset_config(rllib_config)
-    temp_config['env_config']['add_gaussian_noise_action'] = True
-    temp_config['env_config']['add_gaussian_noise_action'] = True
-    gaussian_state_action_rewards, num_steps_GaA_GaS = run_rollout(temp_config, checkpoint, save_trajectory=save_trajectory, video_file="gauss_both",
-                                                show_images=show_images, num_rollouts=num_rollouts)
-    with open(os.path.join(file_path, '{}/{}_gaussian_state_action_noise_rew.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, gaussian_state_action_rewards, delimiter=', ')
-    with open(os.path.join(file_path, '{}/{}_gaussian_state_action_noise_steps.txt'.format(outdir, output_file_name)),
-              'wb') as file:
-        np.savetxt(file, num_steps_GaA_GaS, delimiter=', ')
-    # #
-    print('The average base reward, episode length is {}, {}'.format(np.mean(base_rewards), np.mean(num_steps_base)))
-    print('The average friction reward, episode length is {}, {}'.format(np.mean(friction_rewards), np.mean(num_steps_friction)))
-    print('The average color reward, episode length is {}, {}'.format(np.mean(color_rewards), np.mean(num_steps_colors)))
-    print('The average unrestricted goal region reward, episode length is {}, {}'.format(np.mean(unrestrict_goal_reg_rewards),
-                                                                                         np.mean(num_steps_unrestrict)))
-    print('The average chasing robot reward, episode length is {}, {}'.format(np.mean(chase_robot_rewards), num_steps_chase))
-    print('The average Gaussian state reward, episode length is {}, {}'.format(np.mean(gaussian_state_rewards), num_steps_GaS))
-    print('The average Gaussian action reward, episode length is {}, {}'.format(np.mean(gaussian_action_rewards), num_steps_GaA))
-    print('The average Gaussian state + action reward, episode length is {}, {}'.format(np.mean(gaussian_state_action_rewards), num_steps_GaA_GaS))
+    temp_output = [run_test.remote(test_name=list[0], is_env_config=list[1], config_value=list[2],
+                 params_name=list[3], params_value=list[4],
+                 outdir=outdir, output_file_name=output_file_name,
+                 save_trajectory=save_trajectory, show_images=show_images, num_rollouts=num_rollouts,
+                 rllib_config=rllib_config, checkpoint=checkpoint) for list in run_list]
+    ray.get(temp_output)
 
 
 if __name__ == '__main__':
