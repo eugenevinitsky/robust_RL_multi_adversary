03e7b0581b74b485f2cd372b3ad08ef3eda4ea45
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2019-12-30 23:28:44 -0500

    Commit for 12/30 kl diff experiments

--- run_scripts/run_ma_agent_cluster.sh
+++ run_scripts/run_ma_agent_cluster.sh
@@ -287,14 +287,30 @@
 ######################################################################################################################################
 ## 12/29/19 tests
 
-ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_0ad_PA_h0 --use_s3 --num_samples 1 \
---train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0 --num_adv 0 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
---tmux --start --stop --cluster-name=ev_ma_test1
-#
-ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_10ad_PA_h0_pred_freq1 --use_s3 --num_samples 1 \
---train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0 --num_adv 10 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
---tmux --start --stop --cluster-name=ev_ma_test2
+#ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_0ad_PA_h0 --use_s3 --num_samples 1 \
+#--train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0 --num_adv 0 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
+#--tmux --start --stop --cluster-name=ev_ma_test1
+##
+#ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_10ad_PA_h0_pred_freq1 --use_s3 --num_samples 1 \
+#--train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0 --num_adv 10 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
+#--tmux --start --stop --cluster-name=ev_ma_test2
 #
 #ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_10ad_PA_h0_pred_freq1_switch --use_s3 --num_samples 1 \
 #--train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0 --num_adv 10 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
 #--tmux --start --stop --cluster-name=ev_ma_test3
+
+ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_10ad_PA_h0_pred_freq1_switch --use_s3 --num_samples 1 \
+--train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0 --num_adv 10 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
+--tmux --start --stop --cluster-name=ev_ma_test4
+
+ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_10ad_kd_p001_PA_h0_pred_freq1_switch --use_s3 --num_samples 1 \
+--train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0.001 --num_adv 10 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
+--tmux --start --stop --cluster-name=ev_ma_test5
+
+ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_10ad_kd_p01__PA_h0_pred_freq1_switch --use_s3 --num_samples 1 \
+--train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0.01 --num_adv 10 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
+--tmux --start --stop --cluster-name=ev_ma_test6
+
+ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/ma_crowd.py --exp_title MA_10ad_kd_p1__PA_h0_pred_freq1_switch --use_s3 --num_samples 1 \
+--train_batch_size 30000 --checkpoint_freq 50 --num_iters 800 --kl_diff_weight 0.1 --num_adv 10 --perturb_actions --num_cpus 8 --run_transfer_tests --human_num 0 --grid_search" \
+--tmux --start --stop --cluster-name=ev_ma_test7
