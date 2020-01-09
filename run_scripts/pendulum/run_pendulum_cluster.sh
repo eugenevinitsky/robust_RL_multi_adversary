db5818eedb8e73ca5992e4267ecff55ed596f40e
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-01-09 11:36:45 -0800

    Commit for 1/09 exps

--- run_scripts/pendulum/run_pendulum_cluster.sh
+++ run_scripts/pendulum/run_pendulum_cluster.sh
@@ -2,27 +2,58 @@
 
 ## 1/8/2019
 
+#ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
+#--exp_title pendulum_lstm_test_0p2str --train_batch_size 10000 \
+#--checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
+#--guess_next_state --adv_strength 0.2 --run_transfer_tests --grid_search --use_s3 --use_lstm" --start --stop --tmux --cluster-name=ev_pend_test
+#
+#ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
+#--exp_title pendulum_nolstm_test_0p2str --train_batch_size 10000 \
+#--checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
+#--guess_next_state --adv_strength 0.2 --run_transfer_tests --grid_search --use_s3" --start --stop --tmux --cluster-name=ev_pend_test2
+#
+#ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
+#--exp_title pendulum_lstm_test_0p1str --train_batch_size 10000 \
+#--checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
+#--guess_next_state --adv_strength 0.1 --run_transfer_tests --grid_search --use_s3 --use_lstm" --start --stop --tmux --cluster-name=ev_pend_test3
+#
+#ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
+#--exp_title pendulum_nolstm_test_012str --train_batch_size 10000 \
+#--checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
+#--guess_next_state --adv_strength 0.1 --run_transfer_tests --grid_search --use_s3" --start --stop --tmux --cluster-name=ev_pend_test4
+#
+#ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
+#--exp_title pendulum_nolstm_test_noadv --train_batch_size 10000 \
+#--checkpoint_freq 20 --num_iters 300 --num_cpus 4 --run_transfer_tests\
+# --grid_search --use_s3 --num_adv 0" --start --stop --tmux --cluster-name=ev_pend_test5
+
+## 1/9/2019
 ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
---exp_title pendulum_lstm_test_0p2str --train_batch_size 10000 \
+--exp_title pendulum_lstm_test_0p2str_conc4 --train_batch_size 10000 \
 --checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
---guess_next_state --adv_strength 0.2 --run_transfer_tests --grid_search --use_s3 --use_lstm" --start --stop --tmux --cluster-name=ev_pend_test
+--guess_next_state --adv_strength 0.2 --run_transfer_tests --grid_search --use_s3 --num_concat_states 4" --start --stop --tmux --cluster-name=ev_pend_test1
 
 ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
---exp_title pendulum_nolstm_test_0p2str --train_batch_size 10000 \
+--exp_title pendulum_lstm_test_0p4str_conc4 --train_batch_size 10000 \
 --checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
---guess_next_state --adv_strength 0.2 --run_transfer_tests --grid_search --use_s3" --start --stop --tmux --cluster-name=ev_pend_test2
+--guess_next_state --adv_strength 0.4 --run_transfer_tests --grid_search --use_s3 --num_concat_states 4" --start --stop --tmux --cluster-name=ev_pend_test2
 
 ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
---exp_title pendulum_lstm_test_0p1str --train_batch_size 10000 \
+--exp_title pendulum_lstm_test_0p6str_conc4 --train_batch_size 10000 \
 --checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
---guess_next_state --adv_strength 0.1 --run_transfer_tests --grid_search --use_s3 --use_lstm" --start --stop --tmux --cluster-name=ev_pend_test3
+--guess_next_state --adv_strength 0.6 --run_transfer_tests --grid_search --use_s3 --num_concat_states 4" --start --stop --tmux --cluster-name=ev_pend_test3
 
 ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
---exp_title pendulum_nolstm_test_012str --train_batch_size 10000 \
+--exp_title pendulum_lstm_test_0p2str_conc10 --train_batch_size 10000 \
 --checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
---guess_next_state --adv_strength 0.1 --run_transfer_tests --grid_search --use_s3" --start --stop --tmux --cluster-name=ev_pend_test4
+--guess_next_state --adv_strength 0.2 --run_transfer_tests --grid_search --use_s3 --num_concat_states 10" --start --stop --tmux --cluster-name=ev_pend_test4
 
 ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
---exp_title pendulum_nolstm_test_noadv --train_batch_size 10000 \
---checkpoint_freq 20 --num_iters 300 --num_cpus 4 --run_transfer_tests\
- --grid_search --use_s3 --num_adv 0" --start --stop --tmux --cluster-name=ev_pend_test5
+--exp_title pendulum_lstm_test_0p4str_conc10 --train_batch_size 10000 \
+--checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
+--guess_next_state --adv_strength 0.4 --run_transfer_tests --grid_search --use_s3 --num_concat_states 10" --start --stop --tmux --cluster-name=ev_pend_test5
+
+ray exec ././../../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_pendulum.py \
+--exp_title pendulum_lstm_test_0p6str_conc10 --train_batch_size 10000 \
+--checkpoint_freq 20 --num_iters 300 --num_cpus 4 --num_adv 5 --model_based --guess_adv \
+--guess_next_state --adv_strength 0.6 --run_transfer_tests --grid_search --use_s3 --num_concat_states 10" --start --stop --tmux --cluster-name=ev_pend_test6
