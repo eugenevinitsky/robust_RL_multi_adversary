501e3ffb7591ea662cfb330d7a2f9cd0b3b6d020
Kanaad Parvate <kanaadp@gmail.com>
2020-01-10 17:35:40 -0500

    created Mujoco env as lerrel did in his paper

--- utils/pendulum_env_creator.py
+++ utils/pendulum_env_creator.py
@@ -1,9 +1,19 @@
 import gym
 from envs.inverted_pendulum_env import MAPendulumEnv, PendulumEnv
+from envs.lerrel.adv_inverted_pendulum_env import AdvMAPendulumEnv
+from gym.envs.mujoco.inverted_pendulum import InvertedPendulumEnv
 
 def pendulum_env_creator(env_config):
     if env_config['num_adversaries'] > 0:
         env = MAPendulumEnv(env_config)
+    else:
+        env = PendulumEnv()
+    return env
+
+def lerrel_pendulum_env_creator(env_config):
+    if env_config['num_adversaries'] > 0:
+        env = AdvMAPendulumEnv(env_config)
+        # env = InvertedPendulumEnv()
     else:
         env = PendulumEnv()
     return env
