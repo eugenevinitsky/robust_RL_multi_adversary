7d7da050a75dba8260635bf5eb5a067cf26bf186
Yuqing Du <yuqing_du@hotmail.com>
2020-01-14 10:46:55 -0800

    Only do adaptive kl if not adversary

--- algorithms/custom_ppo.py
+++ algorithms/custom_ppo.py
@@ -371,19 +371,18 @@ def update_kl_and_kl_diff(trainer, fetches):
         trainer.workers.local_worker().foreach_trainable_policy(update)
 
     if "kl" in fetches:
-         # single-agent
-         trainer.workers.local_worker().for_policy(
-             lambda pi: pi.update_kl(fetches["kl"]))
+          # single-agent
+          trainer.workers.local_worker().for_policy(
+              lambda pi: pi.update_kl(fetches["kl"]))
     else:
+          def _update(pi, pi_id):
+              if pi_id in fetches and "adversary" not in pi_id:
+                  pi.update_kl(fetches[pi_id]["kl"])
+              else:
+                  logger.debug("No data for {}, not updating kl".format(pi_id))
 
-         def _update(pi, pi_id):
-             if pi_id in fetches:
-                 pi.update_kl(fetches[pi_id]["kl"])
-             else:
-                 logger.debug("No data for {}, not updating kl".format(pi_id))
-
-         # multi-agent
-         trainer.workers.local_worker().foreach_trainable_policy(_update)
+          # multi-agent
+          trainer.workers.local_worker().foreach_trainable_policy(_update)
 
 CustomPPOPolicy = PPOTFPolicy.with_updates(
     name="POPO",
