6f4678e95e89ada443c15fc19b1807465ec21dc7
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-01-17 17:24:32 -0800

    Fix saving of pkl files

--- run_scripts/pendulum/run_pendulum_lerrel.py
+++ run_scripts/pendulum/run_pendulum_lerrel.py
@@ -1,13 +1,11 @@
 from copy import copy
 import errno
 from datetime import datetime
-import random
 import os
 import subprocess
 import sys
 
 import pytz
-import numpy as np
 import ray
 from ray.rllib.agents.ppo.ppo_policy import PPOTFPolicy
 from ray.rllib.agents.ppo.ppo import PPOTrainer, DEFAULT_CONFIG
@@ -19,7 +17,6 @@ from ray.tune.logger import pretty_print
 from ray.tune import run as run_tune
 from ray.tune.registry import register_env
 
-# from algorithms.custom_ppo import KLPPOTrainer, CustomPPOPolicy, DEFAULT_CONFIG
 
 from visualize.pendulum.transfer_tests import run_transfer_tests
 from visualize.pendulum.visualize_adversaries import visualize_adversaries
@@ -182,32 +179,37 @@ class AlternateTraining(Trainable):
     def _setup(self, config):
         self.config = config
         self.env = config['env']
-
-    def _train(self):
+        self.num_iters = 0
         agent_config = self.config
         adv_config = copy(self.config)
         agent_config['multiagent']['policies_to_train'] = ['pendulum']
         adv_config['multiagent']['policies_to_train'] = ['adversary0']
 
-        agent_trainer = PPOTrainer(env=self.env, config=agent_config)
-        adv_trainer = PPOTrainer(env=self.env, config=adv_config)
+        self.agent_trainer = PPOTrainer(env=self.env, config=agent_config)
+        self.adv_trainer = PPOTrainer(env=self.env, config=adv_config)
 
-        for i in range(args.num_iters):
-            print("== Iteration", i, "==")
+    def _train(self):
 
-            # improve the DQN policy
-            print("-- Adversary Training --")
-            print(pretty_print(adv_trainer.train()))
+        # improve the Adversary policy
+        print("-- Adversary Training --")
+        self.adv_trainer.train()
+        print(pretty_print(self.adv_trainer.train()))
 
-            # swap weights to synchronize
-            agent_trainer.set_weights(adv_trainer.get_weights(["adversary0"]))
-            # improve the PPO policy
-            print("-- Agent Training --")
-            print(pretty_print(agent_trainer.train()))
+        # swap weights to synchronize
+        self.agent_trainer.set_weights(self.adv_trainer.get_weights(["adversary0"]))
+        # improve the Agent policy
+        print("-- Agent Training --")
+        output = self.agent_trainer.train()
+        print(pretty_print(output))
 
-            # swap weights to synchronize
-            adv_trainer.set_weights(agent_trainer.get_weights(["pendulum"]))
-        return {}
+        # swap weights to synchronize
+        self.adv_trainer.set_weights(self.agent_trainer.get_weights(["pendulum"]))
+
+        self.num_iters += 1
+        return output
+
+    def _save(self, tmp_checkpoint_dir):
+        return self.agent_trainer._save(tmp_checkpoint_dir)
 
 
 if __name__ == "__main__":
@@ -251,9 +253,9 @@ if __name__ == "__main__":
                 config, checkpoint_path = get_config_from_path(folder, str(args.num_iters))
 
                 if args.num_adv > 0:
-                    run_transfer_tests(config, checkpoint_path, 200, args.exp_title, output_path)
+                    run_transfer_tests(config, checkpoint_path, 100, args.exp_title, output_path)
 
-                    visualize_adversaries(config, checkpoint_path, 10, 200, output_path)
+                    visualize_adversaries(config, checkpoint_path, 10, 100, output_path)
                     p1 = subprocess.Popen("aws s3 sync {} {}".format(output_path,
                                                                      "s3://sim2real/transfer_results/pendulum/{}/{}/{}".format(date,
                                                                                                                       args.exp_title,
