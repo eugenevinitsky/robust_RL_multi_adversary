e3814f52c58672d7301955a5dd6482c9b16d5553
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-01-22 18:58:19 -0800

    added a file for making the heat map even if things broke

---
+++ run_scripts/make_heatmap.py
@@ -0,0 +1,55 @@
+import argparse
+import errno
+import os
+import subprocess
+
+import ray
+
+
+from utils.rllib_utils import get_config_from_path
+from visualize.pendulum.transfer_tests import run_transfer_tests
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('date', type=str, help='date in format m-d-y')
+    parser.add_argument('exp_title', type=str, help='name of experiment you ran')
+    parser.add_argument('num_iters', type=int, help='How many iterations the exp was run for')
+    args = parser.parse_args()
+
+    ray.init()
+    output_path = os.path.join(os.path.join(os.path.expanduser('~/transfer_results/adv_robust'), args.date), args.exp_title)
+    if not os.path.exists(output_path):
+        try:
+            os.makedirs(output_path)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+    for (dirpath, dirnames, filenames) in os.walk(os.path.expanduser("~/ray_results")):
+        if "checkpoint_{}".format(args.num_iters) in dirpath:
+            # grab the experiment name
+            folder = os.path.dirname(dirpath)
+            tune_name = folder.split("/")[-1]
+            outer_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            script_path = os.path.expanduser(os.path.join(outer_folder, "visualize/transfer_test.py"))
+            config, checkpoint_path = get_config_from_path(folder, str(args.num_iters))
+
+            # TODO(@ev) gross find somewhere else to put this
+
+            if config['env'] == "MALerrelPendulumEnv":
+                from visualize.pendulum.transfer_tests import pendulum_run_list
+                lerrel_run_list = pendulum_run_list
+            elif config['env'] == "MALerrelHopperEnv":
+                from visualize.pendulum.transfer_tests import hopper_run_list
+                lerrel_run_list = hopper_run_list
+
+            run_transfer_tests(config, checkpoint_path, 20, args.exp_title, output_path, run_list=lerrel_run_list)
+
+            # visualize_adversaries(config, checkpoint_path, 10, 100, output_path)
+            p1 = subprocess.Popen("aws s3 sync {} {}".format(output_path,
+                                                             "s3://sim2real/transfer_results/adv_robust/{}/{}/{}".format(args.date,
+                                                                                                              args.exp_title,
+                                                                                                              tune_name)).split(
+                ' '))
+            p1.wait()
