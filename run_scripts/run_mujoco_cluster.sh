8fc2dcf0596c9ef672f1d00fd2ee45f28a26d24e
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-01-23 13:19:35 -0800

    Add action histogram computation to end of run script

--- run_scripts/run_mujoco_cluster.sh
+++ run_scripts/run_mujoco_cluster.sh
@@ -79,10 +79,19 @@ ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts
 # 1 adversary
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_adv_lerrel.py \
 --train_batch_size 10000 --num_iters 1000 --checkpoint_freq 100 --curriculum --num_concat_states 1 \
---num_adv_strengths 1 --advs_per_strength 1 --adv_strength 10.0 --grid_search --use_s3 --env_name hopper \
+--num_adv_strengths 1 --advs_per_strength 1 --adv_strength 5.0 --grid_search --use_s3 --env_name hopper \
 --exp_title hop_1adv_1ps_str10p0_concat1_curr --num_cpus 2 --run_transfer_tests --goal_score 3000 --multi_node" \
 --start --stop --tmux --cluster-name=ev_pend_test2
 
+# 1 adversary no curriculum
+ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_adv_lerrel.py \
+--train_batch_size 10000 --num_iters 1000 --checkpoint_freq 100 --num_concat_states 1 \
+--num_adv_strengths 1 --advs_per_strength 1 --adv_strength 5.0 --grid_search --use_s3 --env_name hopper \
+--exp_title hop_1adv_1ps_str5p0_concat1 --num_cpus 2 --run_transfer_tests --multi_node" \
+--start --stop --tmux --cluster-name=ev_pend_test9
+
+# We test the hypothesis that lots of adversaries in the same strength level is good. They may pick different strategies if they all have
+# the same strength
 # curriculum with 2 adversaries per strength and no memory and low goal
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_adv_lerrel.py \
 --train_batch_size 10000 --num_iters 1000 --checkpoint_freq 100 --curriculum --concat_actions --num_concat_states 1 \
@@ -97,6 +106,7 @@ ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts
 --exp_title hop_2adv_5ps_str5p0_concat1_curr_gs2500 --num_cpus 2 --run_transfer_tests --goal_score 2500 --multi_node" \
 --start --stop --tmux --cluster-name=ev_pend_test4
 
+# Just testing the no curricula case. Same logic in adversary number as above.
 # curriculum with 2 adversaries per strength and no memory and no curriculum
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_adv_lerrel.py \
 --train_batch_size 10000 --num_iters 1000 --checkpoint_freq 100 --concat_actions --num_concat_states 1 \
@@ -111,6 +121,7 @@ ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts
 --exp_title hop_2adv_5ps_str5p0_concat1 --num_cpus 2 --run_transfer_tests --multi_node" \
 --start --stop --tmux --cluster-name=ev_pend_test6
 
+## These test the hypothesis that because of the small batch size, too many adversaries leads to the adversaries not training
 # curriculum with 2 adversaries per strength, 4 adv total and no memory and no curriculum
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_adv_lerrel.py \
 --train_batch_size 10000 --num_iters 1000 --checkpoint_freq 100 --concat_actions --num_concat_states 1 \
