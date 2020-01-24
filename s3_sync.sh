59954da94787d46e88a12233bdd9e3f4d30b39f3
Kanaad Parvate <kanaadp@gmail.com>
2020-01-23 17:17:32 -0800

    added magic s3 script, sweep.rew, and right cheetah_env

---
+++ s3_sync.sh
@@ -0,0 +1,6 @@
+end="$(cut -d'/' -f2 <<<"$1")"
+aws s3 sync s3://sim2real/adv_robust/$1/$end ../sim2real_results/$1 --exclude="*" --include="*event*"
+aws s3 sync s3://sim2real/adv_robust/$1/$end ../sim2real_results/$1 --exclude="*" --include="*params*"
+aws s3 sync s3://sim2real/adv_robust/$1/$end ../sim2real_results/$1 --exclude="*" --include="*checkpoint_500*"
+aws s3 sync s3://sim2real/transfer_results/adv_robust/$1 ../sim2real_results/$1 --exclude="*" --include="*png*"
+aws s3 sync s3://sim2real/transfer_results/adv_robust/$1 ../sim2real_results/$1 --exclude="*" --include="*mean_sweep.txt*"
