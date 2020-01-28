b24e2dfb32e44e5b2d527e392bc2af6194bdcd9f
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-01-27 17:19:51 -0800

    Make s3 sync sync directly to folder without a command line arg

--- run_scripts/s3_sync.sh
+++ run_scripts/s3_sync.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-end="$(cut -d'/' -f2 <<<"$1")"
 mkdir "/home/ubuntu/s3_test"
 cd "/home/ubuntu/s3_test"
-aws s3 sync s3://sim2real/adv_robust/$1/$end ./$1 --exclude="*" --include="*params*"
-aws s3 sync s3://sim2real/adv_robust/$1/$end ./$1 --exclude="*" --include="*checkpoint_1000*"
+aws s3 sync s3://sim2real/adv_robust ./ --exclude="*" --include="*params*"
+aws s3 sync s3://sim2real/adv_robust ./ --exclude="*" --include="*checkpoint_1000*"
