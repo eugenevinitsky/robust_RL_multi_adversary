851fcdf240d1e69fc8710d9d2ea903b55f9c3008
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-01-27 17:09:00 -0800

    fixed the s3 sync script to construct a folder correctly

--- run_scripts/s3_sync.sh
+++ run_scripts/s3_sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 end="$(cut -d'/' -f2 <<<"$1")"
-mkdir "~/s3_test"
-cd s3_data
+mkdir "/home/ubuntu/s3_test"
+cd "/home/ubuntu/s3_test"
 aws s3 sync s3://sim2real/adv_robust/$1/$end ./$1 --exclude="*" --include="*params*"
 aws s3 sync s3://sim2real/adv_robust/$1/$end ./$1 --exclude="*" --include="*checkpoint_1000*"
