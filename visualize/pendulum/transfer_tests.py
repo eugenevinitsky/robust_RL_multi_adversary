a4f84958eae6c7dd2a7c1e3bbf58618f9f5b03fc
Kanaad Parvate <kanaadp@gmail.com>
2020-01-29 02:52:28 -0800

    plotting

--- visualize/pendulum/transfer_tests.py
+++ visualize/pendulum/transfer_tests.py
@@ -205,7 +205,7 @@ def run_transfer_tests(rllib_config, checkpoint, num_rollouts, output_file_name,
             plt.close(fig)
     
     num_advs = rllib_config['env_config']['advs_per_strength'] * rllib_config['env_config']['num_adv_strengths']
-    adv_names = ["adversary{}" for adv_num in range(num_advs)]
+    adv_names = ["adversary{}".format(adv_num) for adv_num in range(num_advs)]
     if num_advs:
         temp_output = [run_test.remote(test_name="adversary{}".format(adv_num),
                     outdir=outdir, output_file_name=output_file_name,
@@ -213,30 +213,29 @@ def run_transfer_tests(rllib_config, checkpoint, num_rollouts, output_file_name,
                     rllib_config=rllib_config, checkpoint=checkpoint, render=render, env_modifier=[], adv_num=adv_num) for adv_num in range(num_advs)]
         temp_output = ray.get(temp_output)
 
-        with open('{}/{}_{}_rew.txt'.format(outdir, output_file_name, "with_adv_mean_sweep.txt"),
+        with open('{}/{}_{}_rew.txt'.format(outdir, output_file_name, "with_adv_mean_sweep"),
                 'wb') as file:
             np.savetxt(file, np.array(temp_output))
 
         with open('{}/{}_{}'.format(outdir, output_file_name, "adv_scores.png"),
                 'wb') as file:
             means = np.array(temp_output)[:,0]
-            
             fig = plt.figure()
-            plt.bar(np.arange(num_advs), adv_names)
+            plt.bar(np.arange(num_advs), means)
             plt.title("Scores under each adversary")
-            plt.xticks(ticks=np.arange(num_advs), labels=adv_names)
+            plt.xticks(np.arange(num_advs), adv_names)
             plt.xlabel("Adv name")
             plt.savefig(file)
             plt.close(fig)
         
         with open('{}/{}_{}'.format(outdir, output_file_name, "adv_steps.png"),
                 'wb') as file:
-            steps = np.array(temp_output)[:,1]
+            steps = np.array(temp_output)[:,2]
             adv_names = ["adversary{}" for adv_num in range(num_advs)]
             fig = plt.figure()
             plt.bar(np.arange(num_advs), steps)
             plt.title("Steps under each adversary")
-            plt.xticks(ticks=np.arange(num_advs), labels=adv_names)
+            plt.xticks(np.arange(num_advs), adv_names)
             plt.xlabel("Adv name")
             plt.savefig(file)
             plt.close(fig)
