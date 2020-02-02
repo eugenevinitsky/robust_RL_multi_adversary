d1ae4586974834c1c403009669e4a4f91f29b5da
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-02-01 17:23:51 -0800

    Minor

--- run_scripts/s3_sync.sh
+++ run_scripts/s3_sync.sh
@@ -3,4 +3,4 @@
 mkdir "/home/ubuntu/s3_test"
 cd "/home/ubuntu/s3_test"
 aws s3 sync s3://sim2real/adv_robust ./ --exclude="*" --include="*params*"
-aws s3 sync s3://sim2real/adv_robust ./ --exclude="*" --include="*checkpoint-1000*"
+aws s3 sync s3://sim2real/adv_robust ./ --exclude="*" --include="*checkpoint-700*"
