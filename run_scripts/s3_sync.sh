c55a29b0a61ee50615bd430fc77e5c6a1f76b977
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-02-01 17:33:45 -0800

    Only sync things by date for s3 sync script

--- run_scripts/s3_sync.sh
+++ run_scripts/s3_sync.sh
@@ -2,5 +2,5 @@
 
 mkdir "/home/ubuntu/s3_test"
 cd "/home/ubuntu/s3_test"
-aws s3 sync s3://sim2real/adv_robust ./ --exclude="*" --include="*params*"
-aws s3 sync s3://sim2real/adv_robust ./ --exclude="*" --include="*checkpoint-700*"
+aws s3 sync s3://sim2real/adv_robust/$1 ./ --exclude="*" --include="*params*"
+aws s3 sync s3://sim2real/adv_robust/$1 ./ --exclude="*" --include="*checkpoint-700*"
