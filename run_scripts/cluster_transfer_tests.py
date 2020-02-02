ca7c9fbc1d55150f1b0bf0050593f394153e1edf
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-02-01 17:33:45 -0800

    Only sync things by date for s3 sync script

--- run_scripts/cluster_transfer_tests.py
+++ run_scripts/cluster_transfer_tests.py
@@ -34,7 +34,7 @@ if not os.path.exists(output_path):
             raise
 
 # now run sync_s3
-p1 = subprocess.Popen(os.path.expanduser("~/adversarial_sim2real/run_scripts/s3_sync.sh"))
+p1 = subprocess.Popen(os.path.expanduser("~/adversarial_sim2real/run_scripts/s3_sync.sh {}".format(date)).split(' '))
 p1.wait()
 
 for (dirpath, dirnames, filenames) in os.walk(os.path.expanduser("~/s3_test")):
