1a0840d20f834cbf5ed87e06929501426219be13
Yuqing Du <yuqing_du@hotmail.com>
2020-04-13 17:22:37 -0700

    Add full transfer tests

--- visualize/plot_heatmap.py
+++ visualize/plot_heatmap.py
@@ -19,8 +19,8 @@ hopper_friction_sweep = np.linspace(0.7, 1.3, 11)
 cheetah_mass_sweep = np.linspace(.5, 1.5, 11)
 cheetah_friction_sweep = np.linspace(0.1, 0.9, 11)
 
-fetch_mass_sweep = np.linspace(.7, 1.3, 1) #TODO: test what values work?
-fetch_friction_sweep = np.linspace(0.7, 1.3, 1)
+fetch_mass_sweep = np.linspace(.7, 1.3, 11) #TODO: test what values work?
+fetch_friction_sweep = np.linspace(0.7, 1.3, 11)
 
 def load_data(results_path):
     all_file_names = OrderedDict()
