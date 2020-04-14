3aa15bf04a6c6b309c64d7087f6670c0a5c9af36
Kanaad Parvate <kanaadp@gmail.com>
2020-04-14 11:29:07 -0700

    fix v function

--- models/rocky_lstm.py
+++ models/rocky_lstm.py
@@ -36,7 +36,7 @@ class RockyLSTM(RecurrentTFModelV2):
         # policy
         # Define input layers
         input_layer = tf.keras.layers.Input(
-            shape=(None, obs_space.shape), name="inputs")
+            shape=(None, obs_space.shape[0]), name="inputs")
         state_in_h = tf.keras.layers.Input(shape=(self.cell_size, ), name="h")
         state_in_c = tf.keras.layers.Input(shape=(self.cell_size, ), name="c")
         seq_in = tf.keras.layers.Input(shape=(), name="seq_in", dtype=tf.int32)
@@ -63,11 +63,14 @@ class RockyLSTM(RecurrentTFModelV2):
         self.rnn_model.summary()
 
         # value fn
+        v_state_in_h = tf.keras.layers.Input(shape=(self.cell_size, ), name="v_h")
+        v_state_in_c = tf.keras.layers.Input(shape=(self.cell_size, ), name="v_c")
+
         v_lstm_out, v_state_h, v_state_c = tf.keras.layers.LSTM(
             self.cell_size, return_sequences=True, return_state=True, name="lstm")(
                 inputs=input_layer,
                 mask=tf.sequence_mask(seq_in),
-                initial_state=[state_in_h, state_in_c])
+                initial_state=[v_state_in_h, v_state_in_c])
 
         # Postprocess LSTM output with another hidden layer and compute values
         values = tf.keras.layers.Dense(
@@ -94,7 +97,7 @@ class RockyLSTM(RecurrentTFModelV2):
         # by subclassing recurrent_tf_modelv2, forward_rnn receives
         # inputs that are B x T x features
         actions, h, c = self.rnn_model([input_dict, seq_lens] + state[0:2])
-        self._value_out, v_h, v_c = self.rnn_model([input_dict, seq_lens] + state[2:4])
+        self._value_out, v_h, v_c = self.value_fn([input_dict, seq_lens] + state[2:4])
         return actions, [h, c, v_h, v_c]
 
     @override(ModelV2)
