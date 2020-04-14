f0cfd1adacad90f9eafc6925fa53fde00b984220
Kanaad Parvate <kanaadp@gmail.com>
2020-04-14 02:50:09 -0700

    remove viz adversaries

--- run_scripts/bandit/run_bernoulli_multiarm_bandit.py
+++ run_scripts/bandit/run_bernoulli_multiarm_bandit.py
@@ -314,7 +314,7 @@ if __name__ == "__main__":
                 ray.shutdown()
                 ray.init()
                 run_transfer_tests(config, checkpoint_path, 1000, args.exp_title, output_path, run_list=make_bernoulli_bandit_transfer_list(args.num_arms))
-                visualize_adversaries(config, checkpoint_path, 100, output_path)
+                # visualize_adversaries(config, checkpoint_path, 100, output_path)
 
                 if args.use_s3:
                     for i in range(4):
