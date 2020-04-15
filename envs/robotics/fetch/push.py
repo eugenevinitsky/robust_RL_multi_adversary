cb169c4c2c96ec90505824673f76404cba535797
Yuqing Du <yuqing_du@hotmail.com>
2020-04-15 16:23:57 -0700

    Add config to push

--- envs/robotics/fetch/push.py
+++ envs/robotics/fetch/push.py
@@ -8,7 +8,7 @@ MODEL_XML_PATH = os.path.join('fetch', 'push.xml')
 
 
 class MAFetchPushEnv(adv_fetch_env.AdvMAFetchEnv, utils.EzPickle):
-    def __init__(self, reward_type='dense'):
+    def __init__(self, config, reward_type='dense'):
         initial_qpos = {
             'robot0:slide0': 0.405,
             'robot0:slide1': 0.48,
@@ -16,7 +16,7 @@ class MAFetchPushEnv(adv_fetch_env.AdvMAFetchEnv, utils.EzPickle):
             'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
         }
         adv_fetch_env.FetchEnv.__init__(
-            self, MODEL_XML_PATH, has_object=True, block_gripper=True, n_substeps=20,
+            self, config, MODEL_XML_PATH, has_object=True, block_gripper=True, n_substeps=20,
             gripper_extra_height=0.0, target_in_the_air=False, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type=reward_type)
