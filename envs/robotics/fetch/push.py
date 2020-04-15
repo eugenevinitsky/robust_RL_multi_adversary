01a6645812c7bc601dd1019bfff20f946f68e578
Yuqing Du <yuqing_du@hotmail.com>
2020-04-15 16:26:46 -0700

    Add config to push

--- envs/robotics/fetch/push.py
+++ envs/robotics/fetch/push.py
@@ -15,7 +15,7 @@ class MAFetchPushEnv(adv_fetch_env.AdvMAFetchEnv, utils.EzPickle):
             'robot0:slide2': 0.0,
             'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
         }
-        adv_fetch_env.FetchEnv.__init__(
+        adv_fetch_env.AdvMAFetchEnv.__init__(
             self, config, MODEL_XML_PATH, has_object=True, block_gripper=True, n_substeps=20,
             gripper_extra_height=0.0, target_in_the_air=False, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
