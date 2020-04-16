bd6d3eaca9a50a8b2f3755b6cf6204784ad783f2
Yuqing Du <yuqing_du@hotmail.com>
2020-04-16 07:39:50 -0700

    chkpt 600

--- run_scripts/s3_sync.sh
+++ run_scripts/s3_sync.sh
@@ -3,4 +3,4 @@
 mkdir "/home/ubuntu/s3_test"
 cd "/home/ubuntu/s3_test"
 aws s3 sync s3://sim2real/adv_robust/$1 ./ --exclude="*" --include="*params*"
-aws s3 sync s3://sim2real/adv_robust/$1 ./ --exclude="*" --include="*checkpoint-700*"
+aws s3 sync s3://sim2real/adv_robust/$1 ./ --exclude="*" --include="*checkpoint-600*"
