0d3353d8ccbeebc9e81b029f76a8819aac643dfe
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-05-13 01:48:31 -0400

    Commit for 5/12 exps with simple adversary reward

--- run_scripts/pendulum/run_adv_lerrel.py
+++ run_scripts/pendulum/run_adv_lerrel.py
@@ -406,6 +406,8 @@ def on_episode_end(info):
             env.extreme_randomize_domain()
         episode = info["episode"]
         episode.custom_metrics["num_active_advs"] = env.adversary_range
+        if env.adversary_range > 0:
+            episode.custom_metrics["adv_{}_rew".format(env.curr_adversary)] = env.total_reward
 
 
 class AlternateTraining(Trainable):
--- run_scripts/run_mujoco_cluster.sh
+++ run_scripts/run_mujoco_cluster.sh
@@ -1002,12 +1002,22 @@
 #--start --stop --tmux --cluster-name=ev_hop_test3
 
 
+############################################################################
+################## 5/11 experiments
+############################################################################
+#ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_adv_lerrel.py \
+#--train_batch_size 100000 --num_iters 500 --checkpoint_freq 100 --num_concat_states 1 --concat_actions \
+#--num_adv_strengths 1 --advs_per_strength 5 --advs_per_rew 1 --num_adv_rews 5 --grid_search --use_s3 --env_name hopper \
+#--exp_title hop_DMALT_5adv_concat1_str0p25_grid_l2000_h4000 --num_cpus 9 --run_transfer_tests --multi_node \
+#--adv_strength 1.0 --adv_all_actions --reward_range --low_reward 2000 --high_reward 4000" \
+#--start --stop --tmux --cluster-name=ev_hop_test3
+
 ###########################################################################
-################# 5/11 experiments
+################# 5/12 experiments
 ###########################################################################
 ray exec ../autoscale.yaml "python /home/ubuntu/adversarial_sim2real/run_scripts/pendulum/run_adv_lerrel.py \
 --train_batch_size 100000 --num_iters 500 --checkpoint_freq 100 --num_concat_states 1 --concat_actions \
 --num_adv_strengths 1 --advs_per_strength 5 --advs_per_rew 1 --num_adv_rews 5 --grid_search --use_s3 --env_name hopper \
---exp_title hop_DMALT_5adv_concat1_str0p25_grid_l2000_h4000 --num_cpus 9 --run_transfer_tests --multi_node \
---adv_strength 1.0 --adv_all_actions --reward_range --low_reward 2000 --high_reward 4000" \
+--exp_title hop_DMALT_5adv_concat1_str0p25_grid_l1000_h4000_srew --num_cpus 9 --run_transfer_tests --multi_node \
+--adv_strength 1.0 --adv_all_actions --reward_range --low_reward 2000 --high_reward 4000 --simple_adv_reward" \
 --start --stop --tmux --cluster-name=ev_hop_test3
