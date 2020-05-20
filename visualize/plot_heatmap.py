5dd3f28d5f650167dff5c1fe496b33e2df0983b0
Yuqing Du <yuqing_du@hotmail.com>
2020-05-20 11:38:25 -0700

    Increase plot bounds for ant

--- visualize/plot_heatmap.py
+++ visualize/plot_heatmap.py
@@ -120,7 +120,7 @@ def save_heatmap(means, mass_sweep, friction_sweep, output_path, file_name, show
         plt.xticks(ticks=np.arange(len(friction_sweep)), labels=["{:0.2f}".format(x) for x in friction_sweep])
         plt.xlabel("Friction coef", fontsize=fontsize)
     elif exp_type == 'ant':
-        plt.imshow(means.T, interpolation='nearest', cmap='seismic', aspect='equal', vmin=400, vmax=6000)
+        plt.imshow(means.T, interpolation='nearest', cmap='seismic', aspect='equal', vmin=400, vmax=8000)
         plt.title(file_name, fontsize=title_fontsize)
         plt.yticks(ticks=np.arange(len(mass_sweep)), labels=["{:0.2f}".format(x * 6.0) for x in mass_sweep])
         plt.ylabel("Mass coef", fontsize=fontsize)
