d6df9c846396ef10b89083db06d4611a6007aaf4
Kanaad Parvate <kanaadp@gmail.com>
2020-05-27 16:07:40 -0400

    visualize adversary perf

--- visualize/bandit/visualize_adversaries.py
+++ visualize/bandit/visualize_adversaries.py
@@ -43,7 +43,7 @@ def visualize_adversaries(rllib_config, checkpoint, num_samples, outdir):
             lambda agent_id: action_init[mapping_cache[agent_id]])
         prev_rewards = collections.defaultdict(lambda: 0.)
         obs = env.reset()['agent']
-        if rllib_config['env'] == 'MultiarmBandit':
+        if rllib_config['env'] == 'BernoulliMultiarmBandit':
             adv_obs = np.array([0.0])
         else:
             adv_obs = obs
@@ -84,84 +84,23 @@ def visualize_adversaries(rllib_config, checkpoint, num_samples, outdir):
             if exc.errno != errno.EEXIST:
                 raise
 
-    if env.num_arms == 1:
-        # Plot the histogram of the actions
-        colors = cm.rainbow(np.linspace(0, 1, num_adversaries))
-        fig = plt.figure(figsize=(8, 8))
-        ax = fig.gca()
-        ax.set_xlim([-1, 1])
-        ax.set_ylim([0, 1])
-        i = 0
-        handle_list = []
-        for adversary, adv_dict in adversary_grid_dict.items():
-            sampled_actions = adv_dict['sampled_actions']
-            sampled_actions_x = [action[0] for action in sampled_actions]
-            sampled_actions_y = [action[1] for action in sampled_actions]
-            handle_list.append(plt.scatter(sampled_actions_x, sampled_actions_y, color=colors[i], label=adversary))
-            plt.title('Scatter of actions over {} sampled obs'.format(num_samples))
-            i += 1
-        output_str = '{}/{}'.format(outdir, 'action_histogram.png')
-        # legends = ['goal'] + ['adversary{}'.format(i) for i in range(len(adversary_grid_dict))]
-        # plt.legend(legends)
-        plt.savefig(output_str)
-        plt.close(fig)
-    elif env.num_arms == 2:
-        # Plot the histogram of the actions
-        colors = cm.rainbow(np.linspace(0, 1, num_adversaries))
-        fig = plt.figure(figsize=(8, 8))
-        ax = fig.gca()
-        ax.set_xlim([-1, 1])
-        ax.set_ylim([-1, 1])
-        i = 0
-        handle_list = []
-        for adversary, adv_dict in adversary_grid_dict.items():
-            sampled_actions = adv_dict['sampled_actions']
-            sampled_actions_x = [action[0] for action in sampled_actions]
-            sampled_actions_y = [action[1] for action in sampled_actions]
-            for action in sampled_actions:
-                spread = Ellipse((action[0], action[1]), action[2]*2, action[3]*2)
-                ax.add_artist(spread)
-                spread.set_alpha(0.1)
-                spread.set_facecolor(colors[i])
-            ax.set_xlabel("Arm 1")
-            ax.set_xlabel("Arm 2")
-            handle_list.append(plt.scatter(sampled_actions_x, sampled_actions_y, color=colors[i], label=adversary))
-            plt.title('Scatter of actions over {} sampled obs'.format(num_samples))
-            i += 1
-        output_str = '{}/{}'.format(outdir, 'action_histogram.png')
-        # legends = ['goal'] + ['adversary{}'.format(i) for i in range(len(adversary_grid_dict))]
-        # plt.legend(legends)
-        plt.savefig(output_str)
-        plt.close(fig)
-
-
     for adv_idx, (adversary, adv_dict) in enumerate(adversary_grid_dict.items()):
-        colors = cm.brg(np.linspace(0, 1, num_arms))
         fig = plt.figure()
         ax = fig.gca()
-        plt.yticks([])
-        ax.set_xlim([-7.5, 7.5])
-        ax.set_ylim([0, 2])
-        ax.set_ylabel("Probability Density", fontsize=10)
-        ax.set_xlabel("Arm Reward", fontsize=10)
-        x = np.arange(-10, 10, 0.001)
-        i = 0
+        ax.set_ylabel("Arms", fontsize=10)
+        ax.set_xlabel("Sampled Step", fontsize=10)
         sampled_actions = adv_dict['sampled_actions']
-        legends = []
-        for arm in range(num_arms):
-            legends.append("Arm {}".format(arm))
-            for action in sampled_actions:
-                arm_spread = norm.pdf(x, action[arm], action[num_arms + arm])
-                ax.fill_between(x, arm_spread, alpha=0.02, color=colors[i])
-                
-            i += 1
-        # plt.title('Arm Distributions for Single Adversary with Regret Target {0:.2f}'.format(np.linspace(-100.0, 0, num_adversaries)[adv_idx]), fontsize=12)
-        plt.title('Arm Distributions for Single Adversary', fontsize=12)
+        heatmap = np.zeros((num_arms, len(sampled_actions)))
+        for i, action in enumerate(sampled_actions):
+            # action = np.sort(action)
+            heatmap[:, i] = action
+
+        plt.title('Arm Distributions for Adversary {}'.format(adv_idx), fontsize=12)
         output_str = '{}/{}_{}'.format(outdir, adversary, 'arm_distribution.png')
-        leg = plt.legend(legends)
-        for i, lh in enumerate(leg.legendHandles): 
-            lh.set_alpha(1)
-            lh.set_color(colors[i])
+        plt.imshow(heatmap, aspect='auto')
+        # plt.colorbar()
+        plt.tight_layout()
+        # plt.show()
         plt.savefig(output_str)
         plt.close(fig)
 
@@ -169,7 +108,7 @@ def main():
     parser = argparse.ArgumentParser('Parse configuration file')
     parser = replay_parser(parser)
     parser.add_argument('--num_samples', type=int, default=100, help='How many observations to sample')
-    parser.add_argument('--output_dir', type=str, default=os.path.expanduser('~/transfer_results/bandit'),
+    parser.add_argument('--output_dir', type=str, default=os.path.expanduser('~/transfer_results/goal_env'),
                         help='Directory to output the files into')
     args = parser.parse_args()
 
