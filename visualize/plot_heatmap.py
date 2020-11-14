07dceeee7bcd08016a844a132e428b1d71e458bd
Yuqing Du <yuqing_du@hotmail.com>
2020-11-13 19:47:12 -0800

    Increase cup mass range

--- visualize/plot_heatmap.py
+++ visualize/plot_heatmap.py
@@ -23,7 +23,7 @@ cheetah_friction_sweep = np.linspace(0.1, 0.9, 11)
 ant_mass_sweep = np.linspace(.5, 1.5, 11)
 ant_friction_sweep = np.linspace(0.1, 0.9, 11)
 
-cup_mass_sweep = np.linspace(.1, 2, 11)
+cup_mass_sweep = np.linspace(.05, 4, 11)
 ball_mass_sweep = np.linspace(0.1, 2, 11)
 
 spinner_mass_sweep = np.linspace(.1, 2, 11)
@@ -106,6 +106,10 @@ def make_heatmap(results_path, exp_type, output_path, show=False, output_file_na
             means = means.reshape(len(ant_mass_sweep), len(ant_friction_sweep))
             save_heatmap(means, ant_mass_sweep, ant_friction_sweep, output_name,
                          output_file_name, show, exp_type, fontsize, title_fontsize)
+        elif exp_type == 'cup':
+            means = means.reshape(len(cup_mass_sweep), len(ball_mass_sweep))
+            save_heatmap(means, cup_mass_sweep, ball_mass_sweep, output_name,
+                         output_file_name, show, exp_type, fontsize, title_fontsize)
 
 
 
@@ -133,6 +137,13 @@ def save_heatmap(means, mass_sweep, friction_sweep, output_path, file_name, show
         plt.ylabel("Mass coef", fontsize=fontsize)
         plt.xticks(ticks=np.arange(len(friction_sweep)), labels=["{:0.2f}".format(x) for x in friction_sweep])
         plt.xlabel("Friction coef", fontsize=fontsize)
+    elif exp_type == 'cup':
+        plt.imshow(means.T, interpolation='nearest', cmap='seismic', aspect='equal', vmin=100, vmax=1000)
+        plt.title(file_name, fontsize=title_fontsize)
+        plt.yticks(ticks=np.arange(len(mass_sweep)), labels=["{:0.2f}".format(x * 6.0) for x in mass_sweep])
+        plt.ylabel("Mass coef", fontsize=fontsize)
+        plt.xticks(ticks=np.arange(len(friction_sweep)), labels=["{:0.2f}".format(x) for x in friction_sweep])
+        plt.xlabel("Friction coef", fontsize=fontsize)
     plt.colorbar()
     plt.tight_layout()
     plt.savefig('{}/{}_{}.png'.format(output_path.replace(' ', '_'), file_name, "transfer_heatmap"))
@@ -143,7 +154,7 @@ def save_heatmap(means, mass_sweep, friction_sweep, output_path, file_name, show
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('results_path', type=str, help='Pass the path to the folder containing all yuor results files')
-    parser.add_argument('exp_type', type=str, help='hopper, cheetah, pendulum, ant')
+    parser.add_argument('exp_type', type=str, help='hopper, cheetah, pendulum, ant, cup')
     parser.add_argument('--output_path', type=str, help='Output file location.')
     parser.add_argument('--show_images', action="store_true", help='Show plots as they are created.')
     args = parser.parse_args()
