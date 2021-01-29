2350ec18a5770bfc5e00fea863a76fa20a09ebd2
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2021-01-28 16:56:54 -0800

    Minor

--- run_scripts/recreate_results/all_cup_exps.sh
+++ run_scripts/recreate_results/all_cup_exps.sh
@@ -31,15 +31,22 @@ ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/run_sc
 --train_batch_size 100000 --num_iters 200 --checkpoint_freq 100 --num_concat_states 1 --num_adv_strengths 0 \
 --advs_per_strength 0 --advs_per_rew 0 --num_adv_rews 0 --use_s3 --env_name cup --exp_title cup_0adv_concat1_grid \
 --num_cpus 10 --run_transfer_tests --multi_node --adv_all_actions --concat_actions \
---seed_search --lambda_val 0.9 --lr .00005" --start --stop --tmux --cluster-name=yd_cup_test1
+--seed_search --lambda_val 0.9 --lr .0005" --start --stop --tmux --cluster-name=yd_cup_test1
 
-# 1 adv grid search
+#ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/visualize/mujoco/cluster_transfer_tests.py cup_1adv_concat1_grid_str0p15 200 01-27-2021" --start --stop --tmux --cluster-name=yd_cup_ttest1
+
+
+#
+## 1 adv grid search
 ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/run_scripts/mujoco/run_adv_mujoco.py \
 --train_batch_size 100000 --num_iters 200 --checkpoint_freq 100 --num_concat_states 1 --num_adv_strengths 1 \
 --advs_per_strength 1 --advs_per_rew 1 --num_adv_rews 1 --use_s3 --env_name cup --exp_title \
 cup_1adv_concat1_grid_str0p15 --num_cpus 10 --run_transfer_tests --multi_node --adv_strength 0.15 \
 --adv_all_actions --concat_actions --clip_actions \
---seed_search --lambda_val 0.9 --lr .00005" --start --stop --tmux --cluster-name=yd_cup_test2
+--seed_search --lambda_val 0.9 --lr .0005" --start --stop --tmux --cluster-name=yd_cup_test2
+
+#ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/visualize/mujoco/cluster_transfer_tests.py cup_1adv_concat1_grid_str0p15 200 01-27-2021" --start --stop --tmux --cluster-name=yd_cup_ttest2
+
 
 # 3 adv grid search w/ no reward ranges
 ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/run_scripts/mujoco/run_adv_mujoco.py \
@@ -47,7 +54,9 @@ ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/run_sc
 --advs_per_strength 3 --advs_per_rew 1 --num_adv_rews 3 --use_s3 --env_name cup \
 --exp_title cup_3adv_concat1_grid_str0p15_norew --num_cpus 10 --run_transfer_tests \
 --multi_node --adv_strength 0.15 --adv_all_actions --concat_actions --clip_actions \
---seed_search --lambda_val 0.9 --lr .00005" --start --stop --tmux --cluster-name=yd_cup_test6
+--seed_search --lambda_val 0.9 --lr .0005" --start --stop --tmux --cluster-name=yd_cup_test6
+
+#ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/visualize/mujoco/cluster_transfer_tests.py cup_3adv_concat1_grid_str0p15_norew 200 01-27-2021" --start --stop --tmux --cluster-name=yd_cup_ttest3
 
 
 # 5 adv grid search w/ no reward ranges
@@ -56,12 +65,15 @@ ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/run_sc
 --advs_per_strength 5 --advs_per_rew 1 --num_adv_rews 5 --use_s3 --env_name cup \
 --exp_title cup_5adv_concat1_grid_str0p15_norew --num_cpus 10 --run_transfer_tests --multi_node \
 --adv_strength 0.15 --adv_all_actions --concat_actions --clip_actions \
---seed_search --lambda_val 0.9 --lr .000005" --start --stop --tmux --cluster-name=yd_cup_test3
+--seed_search --lambda_val 0.9 --lr .0005" --start --stop --tmux --cluster-name=yd_cup_test3
 
+#ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/visualize/mujoco/cluster_transfer_tests.py cup_5adv_concat1_grid_str0p15_norew 200 01-27-2021" --start --stop --tmux --cluster-name=yd_cup_ttest4
 
 # Domain randomization experiment w/ grid search
 ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/run_scripts/mujoco/run_adv_mujoco.py \
 --train_batch_size 100000 --num_iters 200 --checkpoint_freq 100 --num_concat_states 1 --num_adv_strengths 0 \
 --advs_per_strength 0 --advs_per_rew 0 --num_adv_rews 0 --use_s3 --env_name cup --exp_title cup_0adv_concat1_grid_dr \
 --num_cpus 10 --run_transfer_tests --multi_node --domain_randomization --adv_all_actions \
---concat_actions --seed_search --lambda_val 0.9 --lr .00005" --start --stop --tmux --cluster-name=yd_cup_test9
+--concat_actions --seed_search --lambda_val 0.9 --lr .0005" --start --stop --tmux --cluster-name=yd_cup_test9
+
+#ray exec autoscale_yd.yaml "python /home/ubuntu/robust_RL_multi_adversary/visualize/mujoco/cluster_transfer_tests.py cup_0adv_concat1_grid_dr 200 01-27-2021" --start --stop --tmux --cluster-name=yd_cup_ttest5
